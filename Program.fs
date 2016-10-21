6b760b941637fe136d2a866bd729141d673c55e0
Cyclic3 <harlan.connor@gmail.com>
2016-10-21 18:33:38 +0100

    Almost done, just need to test

--- Program.fs
+++ Program.fs
@@ -3,130 +3,52 @@ open System.Numerics
 open System.Net
 open System.Net.Sockets
 #nowarn "9"
-type InfiniteSequence<'T>(f:'T -> uint64 -> 'T,init:'T) = 
-    let mutable i = 0uL
-    let mutable acc = init
-    interface System.Collections.Generic.IEnumerable<'T> with
-        member x.GetEnumerator() = 
-            {
-                new System.Collections.Generic.IEnumerator<'T> with 
-                    member x.Current with get() = acc:'T
-                    member x.Current = acc :> obj
-                    member x.Dispose() = ()
-                    member x.Reset() = i<-0uL;acc<-init
-                    member x.MoveNext() = acc <- f acc i; i <- i+1uL; true
-            }
-        member x.GetEnumerator() = 
-            {
-                new System.Collections.IEnumerator with 
-                    member x.Current = acc :> obj
-                    member x.Reset() = i<-0uL;acc<-init
-                    member x.MoveNext() = acc <- f acc i; i <- i+1uL; true
-            }
-    member x.Current = acc
-    member x.GetNext() = 
-        acc <- f acc i; 
-        i <- i+1uL;
-        acc
 [<EntryPoint>]
 let main  = 
     function 
     |[|ipaddress;port|] ->
+        let real i = Complex(i, 0.)
+        let imaginary i = Complex(0., i)
+
         let client = new TcpClient()
         client.Connect(IPAddress.Parse ipaddress, int port)
+
         while client.Available = 0 do ()
         let b = Array.zeroCreate<byte> 44
         client.Client.Receive(b) |> ignore
-        let startx = System.BitConverter.ToDouble(b,0)
-        let starty = System.BitConverter.ToDouble(b,8)
-        let finishx = System.BitConverter.ToDouble(b,16)
-        let finishy = System.BitConverter.ToDouble(b,24)
-        let res = System.BitConverter.ToDouble(b,32)
+
+        let startx = real <| System.BitConverter.ToDouble(b,0)
+        let starty = imaginary <| System.BitConverter.ToDouble(b,8)
+        let finishx = real <| System.BitConverter.ToDouble(b,16)
+        let finishy = imaginary <| System.BitConverter.ToDouble(b,24)
+        let res = real <| System.BitConverter.ToDouble(b,32)
         let iters = System.BitConverter.ToInt32(b,40)
-        printfn "From (%f, %f) to (%f, %f) in %f steps" startx starty finishx finishy res
-        let width    = abs(startx) + abs(finishx)
-        let height = abs(starty) + abs(finishy)
-        let x = width    / res |> int
-        let y = height / res |> int
+
+        printfn "From %A to %A in steps of %A" (startx + starty) (finishx + finishy) res
+
+        let width    = abs(startx.Magnitude) + abs(finishx.Magnitude)
+        let height = abs(starty.Magnitude) + abs(finishy.Magnitude)
+
+        let x = width  / res.Magnitude |> int
+        let y = height / res.Magnitude |> int
+
         let size = int64 x * int64 y
 
         let real i = Complex(i, 0.)
         let imaginary i = Complex(0., i)
 
         let seed = real 0.
-        let n_bad = ref 0L
-        let isGood (n:Complex) = 
-            let mag = n.Magnitude
-            let ans = (mag = infinity || mag <> mag) |> not
-            if not ans then System.Threading.Interlocked.Increment(n_bad) |> ignore
-            ans
-        let orbitfunc c = 
-            InfiniteSequence<_>(
-                (fun x _ -> if isGood x then (x*x) + c else real infinity),
-                seed
+        let isGood (n:Complex) = (n.Real <> infinity && n.Imaginary <> infinity) //nan is not an issue, unless we messed up
+        Array.Parallel.init x (fun i -> 
+            let start = startx + (real (float i) * res)
+            Array.Parallel.init y (fun j ->
+                let rec check c v = function |n when n = iters -> n |n -> if isGood v then check c ((v * v)+c) (n+1) else n
+                let init = start + (starty + (real (float j) * res))
+                check init init 0
+                |> System.BitConverter.GetBytes
             )
-        let mandelbrot res (startx,finishx) (starty,finishy) = 
-            let x' = Array.Parallel.init x (fun i -> (float i * res + startx))
-            let y' = Array.Parallel.init y (fun i -> (float i * res + starty))
-            let cat = ref size
-            let t = 
-                async{
-                    let mouse = ref size
-                    while !cat > 0L do
-                        if (!mouse - !cat) > size/100L then 
-                            mouse := !cat
-                            printfn "\t%i%% complete" ((size - !mouse) / (size/100L))
-                } |> Async.StartAsTask
-            Array.Parallel.map(fun i -> 
-                Array.Parallel.map (fun j ->
-                    System.Threading.Interlocked.Decrement(cat)|>ignore
-                    Complex(i,j)|>orbitfunc
-                ) y'
-            ) x'
-            |> fun i -> t.Wait();i
-        let iter_all f v = Array.map(Array.map(fun (i:InfiniteSequence<_>) -> i.GetNext()|>f)) v
-
-        let thing(i:byte) = 
-            if i < 128uy then i*2uy
-            else 255uy-(i*2uy)
-        let get_nice_colour j min max=
-            let H = (float max - float j) * 270. / (float max - float min)
-            //http://www.rapidtables.com/convert/color/hsv-to-rgb.htm
-            let X = 1. - abs(((H/60.) % 2.) - 1.)
-            let r',g',b' = 
-                if   H < 60.  then 1.,X ,0.
-                elif H < 120. then X ,1.,0.
-                elif H < 180. then 0.,1.,X
-                elif H < 240. then 0.,X ,1.
-                elif H < 300. then X ,0.,1.
-                else                 1.,0.,X
-            printfn "%A" (r',g',b')
-            r',g',b'
-        let rec progress i stop mb (res:(float*float*float)[][]) =
-            if i <= stop then
-                printf "%i -> " i
-                n_bad := 0L
-                let d' = iter_all isGood mb 
-                printfn "[%i/%i] " !n_bad size
-                let c = get_nice_colour i 0 stop 
-                Array.Parallel.iteri (fun x a ->
-                    Array.Parallel.iteri (fun y ->
-                        function 
-                        |true -> res.[x].[y] <- c
-                        |_ -> ()
-                    ) a
-                ) d'
-                printfn "%f%% complete" ((float i / float stop)*100.)
-                progress (i+1) stop mb res
-            else res
-        printfn "%ix%i\nGenerating set..." x y
-        let mb = mandelbrot res (startx,finishx) (starty,finishy)
-        printfn "done"
-        progress 0 iters mb (Array.Parallel.init x (fun _ -> Array.zeroCreate y))
-        |> Array.Parallel.map (Array.Parallel.map (fun (r,g,b) -> Array.concat [|System.BitConverter.GetBytes r; System.BitConverter.GetBytes g; System.BitConverter.GetBytes g|]))
-        |> Array.concat
+            |> Array.concat
+        )
         |> Array.concat
         |> client.Client.Send
-        |> printfn "Sent %i bytes"
-        0
     |_ -> printfn "Mandelbrot <ip address> <port>"; exit -1
--- Server/Program.fs
+++ Server/Program.fs
@@ -2,15 +2,75 @@
 // See the 'F# Tutorial' project for more help.
 open System.Net
 open System.Net.Sockets
+open System.Numerics
 [<EntryPoint>]
 let main = function
-    |[|x;y|] ->
-        let x,y = int x, int y
+    |[|startx;starty;finishx;finishy;res;iters;alloc;port|] ->
+        let port     = int port
+        let res      = float res
+        let alloc    = float alloc
+        let iters    = int iters
+        let listener = TcpListener(IPEndPoint(IPAddress.Any, port))
+        let startx   = float startx
+        let starty   = float starty
+        let finishx  = float finishx
+        let finishy  = float finishy
+
+        printfn "Bound to port %i" port
+        let start  = Complex(startx, starty)
+        let finish = Complex(finishx, finishy)
+        let width  = finish.Real - start.Real
+        let height = finish.Imaginary - start.Imaginary
+        let x = int(width/alloc)
+        let y = int(height/alloc)
+        let step = int(alloc/res)
+
         //You better have a lot of swap m8
-        let bm = new System.Drawing.Bitmap(x,y)
-        let drawone (r:float,g:float,b:float) (x,y) = 
+        let bm = new System.Drawing.Bitmap(x*step,y*step)
+
+        let drawone i (x,y) = 
+            let r,g,b =
+                let H = (iters - i) * 270 / iters
+                //http://www.rapidtables.com/convert/color/hsv-to-rgb.htm
+                let X = 1. - abs(((float H/60.) % 2.) - 1.)
+                if   H < 60  then 1.,X ,0.
+                elif H < 120 then X ,1.,0.
+                elif H < 180 then 0.,1.,X
+                elif H < 240 then 0.,X ,1.
+                elif H < 300 then X ,0.,1.
+                else              1.,0.,X
             let c = System.Drawing.Color.FromArgb(r*255. |> int, g*255. |> int, b*255. |> int)
             lock bm (fun () -> bm.SetPixel(x,y,c))
+        
+        let xarr = [|0..x-1|]
+        let yarr = [|0..y-1|] //a pirate I be
+        Array.Parallel.iter (fun i ->
+            let startx = float i * alloc
+            let finishx = (float i * alloc) + (float step) - 1.
+            Array.Parallel.iter (fun j ->
+                let starty = float j * alloc
+                let finishy = (float j * alloc) + (float step) - 1.
+                let s = listener.AcceptTcpClient()
+                startx  |> System.BitConverter.GetBytes |> s.Client.Send |> ignore
+                starty  |> System.BitConverter.GetBytes |> s.Client.Send |> ignore
+                finishx |> System.BitConverter.GetBytes |> s.Client.Send |> ignore
+                finishy |> System.BitConverter.GetBytes |> s.Client.Send |> ignore
+                res     |> System.BitConverter.GetBytes |> s.Client.Send |> ignore
+                iters   |> System.BitConverter.GetBytes |> s.Client.Send |> ignore
+                let width1 = (abs startx + abs finishx)/res |> int
+                while s.Available = 0 do () done
+                let b = Array.zeroCreate<byte> s.Available
+                s.Client.Receive(b) |> ignore
+                b
+                |> Array.chunkBySize 4
+                |> Array.Parallel.map (fun i -> System.BitConverter.ToInt32(i,0))
+                |> Array.chunkBySize width1
+                |> Array.iteri (fun i -> 
+                    Array.iteri(fun j v -> 
+                        drawone v (i,j))
+                    )
+            ) yarr
+        ) xarr
         0
-    |_ -> printfn "Server <x> <y>"; exit -1
+    |_ -> printfn "Server <resolution> <iterations> <allocation size> <port>"; exit -1
 
